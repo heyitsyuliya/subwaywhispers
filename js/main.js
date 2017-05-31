2036f4293b9f0602e30089987efde16b4b891c49
Yuliya Kandratsyeva <ykandratsyeva@gmail.com>
2017-05-30 21:14:24 -0400

    more stuff + fixed scroll function

--- js/main.js
+++ js/main.js
@@ -4,30 +4,17 @@ $(function() {
 	// dropdown functionality
 	$('.dropdown-content').hide()
 
+	// events
+	$('.nav-links > ul > li:nth-child(1) > a').on('click', slideContent)
+	$('.nav-links > ul > li:nth-child(2) > a').on('click', slideContent)
 
-	//when about was clicked
-	$('.nav-links > ul > li:nth-child(1) > a, .nav-links > ul > li:nth-child(3) > a').on('click', function(e){
-		e.preventDefault();
+	// functions
+	function slideContent(event){
+
+		event.preventDefault()
 		var target = $(this).attr('href'),
 			offsetTop = $(target).offset().top;
 
-		$("html,body").animate({scrollTop:offsetTop}, 800)
-	});
-
-	// // events
-	// $('.nav-links > ul > li:nth-child(1) > a').on('click', slideContent(event))
-	// $('.nav-links > ul > li:nth-child(3) > a').on('click', slideContent(event))
-
-	// // functions
-
-	// function slideContent(event){
-
-	// 	event.preventDefault()
-	// 	var target = $(this).attr('href'),
-	// 		offsetTop = $(target).offset().top;
-
-	// 	console.log(target)
-	// 	console.log(offsetTop)
-	// 	$('html, body').animate({scrollTop:offsetTop}, 800)
-	// }
+		$('html, body').animate({scrollTop:offsetTop}, 800)
+	}
 });
